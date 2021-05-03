9f6cc985e9e97b4f20d78c973b2930f4ab2ec77a
Brian Li <brian@brianli.com>
2021-05-03 13:51:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const balancedLoansAddress = 'cx66d4d90f5f113eba575bf793570135f9b10cece1'
 const balancedDexAddress = 'cxa0af3165c08318e988cb30993b3048335b94af6c'
 const balancedRewardsAddress = 'cx10d59e8103ab44635190bd4139dbfd682fa2d07e'
 const balnTokenAddress = 'cxf61cd5a45dc9f91c15aa65831a30a90d59a09619'
+const bandOracleAddress = 'cx087b4164a87fdfb7b714f3bafe9dfb050fd6b132'
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event))
@@ -12,7 +13,7 @@ async function gatherResponse(response) {
   return JSON.stringify(await response.json())
 }
 
-async function icx_call(contractAddress, method, params) {
+async function icxCall(contractAddress, method, params) {
   const payload = {
     jsonrpc: '2.0',
     method: 'icx_call',
@@ -39,24 +40,56 @@ async function icx_call(contractAddress, method, params) {
   return json.result
 }
 
+async function icxGetBalance(address) {
+  const payload = {
+    jsonrpc: '2.0',
+    method: 'icx_getBalance',
+    id: 1234,
+    params: {
+      address: address,
+    },
+  }
+  const init = {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+    method: 'POST',
+    body: JSON.stringify(payload),
+  }
+  const response = await fetch(icxApiEndpoint, init)
+  const results = await gatherResponse(response)
+  json = JSON.parse(results)
+  return json.result
+}
+
+async function getTvl() {
+  balancedBalance = await icxGetBalance(balancedDexAddress)
+  bandOracleCall = await icxCall(bandOracleAddress, 'get_ref_data', {
+    _symbol: 'ICX',
+  })
+  tvlIcx = await hexToInt(balancedBalance, 18)
+  icxPrice = parseInt(bandOracleCall.rate, 16) / 1000000000
+  return { tvlIcx: tvlIcx, tvlUsd: tvlIcx * icxPrice }
+}
+
 async function hexToInt(hex, dec) {
   return parseInt(hex, 16) / 10 ** dec
 }
 
 async function getApy(pool) {
-  apy = await icx_call(balancedRewardsAddress, 'getAPY', { _name: pool })
+  apy = await icxCall(balancedRewardsAddress, 'getAPY', { _name: pool })
   return hexToInt(apy, 16)
 }
 
 async function getPriceByName(pool) {
-  price = await icx_call(balancedDexAddress, 'getPriceByName', {
+  price = await icxCall(balancedDexAddress, 'getPriceByName', {
     _name: pool,
   })
   return hexToInt(price, 18)
 }
 
 async function getPoolStats(poolId) {
-  poolStats = await icx_call(balancedDexAddress, 'getPoolStats', {
+  poolStats = await icxCall(balancedDexAddress, 'getPoolStats', {
     _id: poolId,
   })
   if (poolId == '1') {
@@ -73,8 +106,8 @@ async function getPoolStats(poolId) {
 }
 
 async function getBalnSupply(type) {
-  totalBalnSupply = await icx_call(balnTokenAddress, 'totalSupply', {})
-  stakedBalnSupply = await icx_call(balnTokenAddress, 'totalStakedBalance', {})
+  totalBalnSupply = await icxCall(balnTokenAddress, 'totalSupply', {})
+  stakedBalnSupply = await icxCall(balnTokenAddress, 'totalStakedBalance', {})
   return {
     totalBalnSupply: await hexToInt(totalBalnSupply, 18),
     stakedBalnSupply: await hexToInt(stakedBalnSupply, 18),
@@ -99,6 +132,7 @@ async function buildJsonResponse() {
       sicxBnusdPool: await getPoolStats('2'),
       sicxIcxPool: await getPoolStats('1'),
     },
+    tvl: await getTvl(),
   }
   return response
 }
@@ -115,6 +149,7 @@ async function handleRequest(event) {
     let headers = new Headers()
     headers.set('Content-Type', 'application/json')
     headers.set('Cache-Control', 's-maxage=10')
+    headers.set('Access-Control-Allow-Origin', '*')
     response = new Response(JSON.stringify(response_payload), {
       headers: headers,
     })
